9e71349aa7684c3ffea6bfa07bc37637f5fe6c88
Brendan Whelan <whelan_brendan@hotmail.com>
2024-01-07 22:19:15 +0000

    Feat:Update JS and currency rates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 //Fetching elements required
-let valEntered = document.getElementById("amount").value;
-let valFrom = document.getElementById("convertFrom").value;
-let valTo = document.getElementById("convertTo").value;
+let amtlEntered = document.getElementById("amount").value;
+let curencyFrom = document.getElementById("convertFrom").value;
+let currencyTo = document.getElementById("convertTo").value;
 
 
 
