248a24c044040db2addd9f5758483df022561f9a
Brendan Whelan <whelan_brendan@hotmail.com>
2024-01-07 19:22:55 +0000

    Feat:More JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 //Fetching elements required
 let valEntered = document.getElementById("amount").value;
+let valFrom = document.getElementById("convertFrom").value;
+let valTo = document.getElementById("convertTo").value;
 
 
 
